619b7ade9cf8c8be97c0d0207318611edbced3f4
lizard-electronics <rod.lagarto@hotmail.com>
2024-04-15 01:47:55 +0100

    Added camera feed to sim env

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -10,51 +10,51 @@ from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
-	# Check if we're told to use sim time
-	use_sim_time = LaunchConfiguration('use_sim_time')
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-	# Process the URDF file
-	pkg_path = os.path.join(get_package_share_directory('lizardbot1'))
-	xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-	robot_description_config = xacro.process_file(xacro_file)
-	rviz_config_path = os.path.join(pkg_path,'config','urdf_config.rviz')
-	
-	# Create a robot_state_publisher node
-	params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-	node_robot_state_publisher = Node(
-		package='robot_state_publisher',
-		executable='robot_state_publisher',
-		output='screen',
-		parameters=[params]
-	)
-	'''
-	# Create a joint_state_publisher_gui node
-	joint_state_publisher_gui_node = Node(
-		package='joint_state_publisher_gui',
-		executable='joint_state_publisher_gui',
-		name='joint_state_publisher_gui'
-	)
-	'''
-	# Create a rviz2 node		
-	rviz_node = Node(
-		package='rviz2',
-		executable='rviz2',
-		name='rviz2',
-		output='screen',
-		arguments=['-d', LaunchConfiguration('rvizconfig')]
-	)
-	# Launch
-	return LaunchDescription([
-		DeclareLaunchArgument(
-			'use_sim_time',
-			default_value='false',
-			description='Use sim time if true'),
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('lizardbot1'))
+    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    rviz_config_path = os.path.join(pkg_path,'config','urdf_config.rviz')
+    
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+    '''
+    # Create a joint_state_publisher_gui node
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
+    '''
+    # Create a rviz2 node		
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')]
+    )
+    # Launch
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
 
-		DeclareLaunchArgument(name='rvizconfig', 
-							  default_value=rviz_config_path,
-							  description='Absolute path to rviz config file'),
+        DeclareLaunchArgument(name='rvizconfig', 
+                            default_value=rviz_config_path,
+                            description='Absolute path to rviz config file'),
 
-		node_robot_state_publisher,
-		# joint_state_publisher_gui_node,
-		rviz_node
-	])
+        node_robot_state_publisher,
+        # joint_state_publisher_gui_node,
+        rviz_node
+    ])
